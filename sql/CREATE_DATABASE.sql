f8722b548735b440410983fc424445fde869f08f
Archie <arch817@gmail.com>
2014-04-08 12:45:33 +0300

    add sql files to task1

---
+++ sql/CREATE_DATABASE.sql
@@ -0,0 +1,12 @@
+﻿-- Database: egov_test
+
+-- DROP DATABASE egov_test;
+
+CREATE DATABASE egov_test
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Russian_Russia.1251'
+       LC_CTYPE = 'Russian_Russia.1251'
+       CONNECTION LIMIT = -1;
+
---
+++ sql/CREATE_PERSON_TABLE.sql
@@ -0,0 +1,23 @@
+﻿-- Table: person
+
+-- DROP TABLE person;
+
+CREATE TABLE person
+(
+  first_name character varying(20) NOT NULL,
+  last_name character varying(20) NOT NULL,
+  patronymic character varying(20) NOT NULL,
+  birthday_date date NOT NULL,
+  location_address character varying(40) NOT NULL,
+  birth_address character varying(40),
+  document_number character varying(20) NOT NULL,
+  issuance_document_date date NOT NULL,
+  expiration_document_date date NOT NULL,
+  document_issued character varying(20) NOT NULL,
+  CONSTRAINT person_pkey PRIMARY KEY (first_name, last_name)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE person
+  OWNER TO postgres;
